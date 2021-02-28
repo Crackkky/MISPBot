d13ddf969321ff2fe5f8de5adcaa01a0b24a0875
Anigionn <arnogarrigue@gmail.com>
2021-02-28 15:57:33 +0100

    1.0 !

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def httpPost(url, headersP, bodyP):
 
 def getEvents():
     headers = {'Authorization': MISP_KEY, 'Accept': 'application/json', 'Content-type': 'application/json'}
-    body = {"returnFormat": "json", "last": TIME_REQUEST+"m"}
+    body = {"returnFormat": "json", "last": TIME_REQUEST + "m"}
     result = httpPost(API_URL, headers, body)
     response = json.loads(result)
     i = 0
@@ -66,18 +66,18 @@ def getTweetPrefix(e):
     if e.timestampUpdate == e.firstUpdate:
         return ""
     else:
-        return "Update!"
+        return "Update! \n"
 
 
 def getTweetSuffix(e):
-    if (len(e.infoEvent) + len(e.creatorOrgName) + len(str(e.firstUpdate)) + 26) < 280:
-        return f"has been attacked : {e.infoEvent}"
+    if (len(e.infoEvent) + len(e.creatorOrgName) + len(str(e.firstUpdate)) + 25) < 280:
+        return f", has been attacked : {e.infoEvent}"
     elif "phishing" in e.infoEvent:
-        return f"has been targeted by a phishing campaign"
+        return f", has been targeted by a phishing campaign"
     elif "malware" in e.infoEvent:
-        return f"has been targeted by a malware attack"
+        return f", has been targeted by a malware attack"
     else:
-        return f". No more information could be given"
+        return f".\nNo more information could be given"
 
 
 def addOrg(data, name):
@@ -111,25 +111,46 @@ def tweetEvents(apiTweet):
     org = {}
     for e in events:
         if e.isPublished:
+
             with open('weekly.json') as json_file:
                 weeklyData = json.load(json_file)
-
             orgExist, org = testOrg(weeklyData, e.creatorOrgName, org)
             updateOrg(orgExist, e, org, weeklyData)
-
             with open('weekly.json', 'w') as outfile:
                 json.dump(weeklyData, outfile, ensure_ascii=False)
 
             try:
                 apiTweet.update_status(
-                    f"{getTweetPrefix(e)} {e.creatorOrgName}, the {e.firstUpdate}, {getTweetSuffix(e)}")
+                    f"{getTweetPrefix(e)} {e.creatorOrgName}, the {e.firstUpdate} {getTweetSuffix(e)}")
             except tweepy.error.TweepError as twperr:
                 print(twperr.reason)
                 pass
 
 
+def sortOrgDict(orgDict):
+    return sorted(orgDict.items(), key=lambda item: item[1])
+
+
 def weeklyTweet(apiTweet):
     print("___Weekly tweet___")
+
+    with open('weekly.json') as json_file:
+        weeklyData = json.load(json_file)
+        tabOrg = {}
+        for org in weeklyData['Orgs']:
+            tabOrg[org['name']] = org['nbEvent']
+
+    tabOrg = sortOrgDict(tabOrg)
+    tabOrg.reverse()
+
+    try:
+        apiTweet.update_status("Weekly report : \n\n"
+                               "The company that reported the most attacks is : " + tabOrg[0][0] + "\nWith : " + str(
+                                tabOrg[0][1]) + " event" + ("s" if tabOrg[0][1] > 1 else "") + " reported this week!")
+
+    except tweepy.error.TweepError as twperr:
+        print(twperr.reason)
+        pass
 
 
 if __name__ == '__main__':
@@ -141,8 +162,9 @@ if __name__ == '__main__':
     api = tweepy.API(auth)
 
     tweetEvents(api)
-    schedule.every(int(TIME_REQUEST)).minutes.do(tweetEvents, api)
-    # schedule.every().day.at("10:30").do(weeklyTweet, api)
+    # schedule.every(int(TIME_REQUEST)).minutes.do(tweetEvents, api)
+    # schedule.every(2).seconds.do(weeklyTweet, api)
+    schedule.every().weeks.at("10:30").do(weeklyTweet, api)
 
     while 1:
         schedule.run_pending()
